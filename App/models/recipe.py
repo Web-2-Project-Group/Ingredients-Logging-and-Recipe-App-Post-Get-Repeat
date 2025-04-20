cbfafbf3a49148b908881722a2f10e256433e2bb
Stardrake95 <157759631+Stardrake95@users.noreply.github.com>
2025-04-19 22:45:00 -0400

    Update recipe.py
    Added description and cook time columns

--- App/models/recipe.py
+++ App/models/recipe.py
@@ -4,26 +4,28 @@ class Recipe(db.Model):
     _tablename_ = 'recipe'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    #cook_time = db.Column(db.String(500),nullable=False)
     number_of_ingredients = db.Column(db.String(900), nullable=False)
     ingredients = db.Column(db.String(900), nullable=False)
     instructions = db.Column(db.String(900), nullable=False)
     image = db.Column(db.String(256), nullable=True)
+    description = db.Column(db.String(256), nullable=False)
+    cook_time = db.Column(db.String(32), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
-    def __init__(self, title,number_of_ingredients, ingredients, instructions, image=None, user_id=None):
+    def __init__(self, title,number_of_ingredients, ingredients, instructions, image=None, user_id=None, description=description, cook_time=cook_time):
         self.title = title
-        #self.cook_time = cook_time
+        self.cook_time = cook_time
         self.number_of_ingredients = number_of_ingredients
-        self.ingredients = ';;'.join(ingredients)  # Double semicolon delimiter
-        self.instructions = '||'.join(instructions)  # Double pipe delimiter
+        self.ingredients = ';'.join(ingredients)  # Single semicolon delimiter
+        self.instructions = ';'.join(instructions)  # Single semicolon delimiter
         self.image = image
+        self.description = description
         self.user_id = user_id
     
     def get_ingredients(self):
         """Return ingredients as list"""
-        return self.ingredients.split(';;')
+        return self.ingredients.split(';')
     
     def get_instructions(self):
         """Return instructions as list"""
-        return self.instructions.split('||')
+        return self.instructions.split(';')
