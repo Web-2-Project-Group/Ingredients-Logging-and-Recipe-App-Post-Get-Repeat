76cb68f267cdb7ed590e113898ed674b56db5a8a
r7pt <125412480+r7pt@users.noreply.github.com>
2025-04-20 22:34:54 -0400

    Update daily_recipe.py

--- App/views/daily_recipe.py
+++ App/views/daily_recipe.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity, current_user
+from App.models import Recipe,User,Review,Inventory
 
 daily_recipe_views = Blueprint('daily_recipe_views', __name__, template_folder='../templates')
 
 @daily_recipe_views.route('/daily-recipes')
 @jwt_required()
 def daily_recipes_page():
-    return render_template('dailyrecipe.html')
+    dailyrecipes = User.query.filter_by(id=current_user.id).first()
+    return render_template('dailyrecipe.html', dailyrecipes=dailyrecipes)
