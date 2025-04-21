50f54dfd2263697060cca5cbdaa51e3e8df10855
r7pt <125412480+r7pt@users.noreply.github.com>
2025-04-20 22:34:00 -0400

    Update inventory.py

--- App/views/inventory.py
+++ App/views/inventory.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 from flask_jwt_extended import jwt_required, current_user
+from App.models import Recipe,User,Review,Inventory
 
 inventory_views = Blueprint('inventory_views', __name__, template_folder='../templates')
 
 @inventory_views.route('/my-ingredients')
 @jwt_required()
 def my_ingredients_page():
-    return render_template('ingredients.html')
+    user = Inventory.query.all()
+    return render_template('ingredients.html', user=user)
+
