7f6acdde0f489c53a3e2e2e905dbaa3e239d6e90
Stardrake95 <157759631+Stardrake95@users.noreply.github.com>
2025-04-21 23:28:19 -0400

    Update Drecipes.py
    Daily Recipe should be functional now

--- App/models/Drecipes.py
+++ App/models/Drecipes.py
@@ -4,8 +4,7 @@ class Drecipes(db.Model):
     _tablename_ = 'Drecipes'
     id = db.Column(db.Integer, primary_key=True)
     recipe_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    date = db.Column(db.DateTime,nullable=False)
+    date = db.Column(db.DateTime,nullable=False, server_default=db.func.now())
 
     def __init__(self, recipe_id):
         self.recipe_id = recipe_id
-        self.date = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
