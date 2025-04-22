74d8c23930871915111fd72af3ce3ecd943fed3e
Stardrake95 <157759631+Stardrake95@users.noreply.github.com>
2025-04-21 23:30:17 -0400

    Update Drecipes.py

--- App/models/Drecipes.py
+++ App/models/Drecipes.py
@@ -4,7 +4,7 @@ class Drecipes(db.Model):
     _tablename_ = 'Drecipes'
     id = db.Column(db.Integer, primary_key=True)
     recipe_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    date = db.Column(db.DateTime,nullable=False, server_default=db.func.now())
+    date = db.Column(db.DateTime, server_default=db.func.now())
 
     def __init__(self, recipe_id):
         self.recipe_id = recipe_id
